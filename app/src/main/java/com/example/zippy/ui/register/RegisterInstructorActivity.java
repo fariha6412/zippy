0dfe3721aa5e738e9f4b326eeab57c70a0c1eccf
fariha6412 <sifupechu@gmail.com>
2021-06-05 18:07:57 +0600

    after creation of instructor and student profile half with everything ok 3

--- app/src/main/java/com/example/zippy/ui/register/RegisterInstructorActivity.java
+++ app/src/main/java/com/example/zippy/ui/register/RegisterInstructorActivity.java
@@ -2,7 +2,6 @@ package com.example.zippy.ui.register;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -25,8 +24,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.regex.Pattern;
 
 public class RegisterInstructorActivity extends AppCompatActivity {
 
