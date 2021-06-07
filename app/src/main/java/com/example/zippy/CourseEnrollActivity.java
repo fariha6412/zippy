aaa86401331bfc013cca73c683ae465e1b8a95d4
fariha6412 <sifupechu@gmail.com>
2021-06-07 17:05:33 +0600

    after modification of CourseCreationActivity and CourseEnrollActivity , everything's ok

--- app/src/main/java/com/example/zippy/CourseEnrollActivity.java
+++ app/src/main/java/com/example/zippy/CourseEnrollActivity.java
@@ -29,7 +29,8 @@ import org.jetbrains.annotations.NotNull;
 public class CourseEnrollActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseDatabase rootNode;
-    DatabaseReference referenceStudent, referenceStudentNoOfCourses, referenceStudentCourses, referenceCourse, referenceCourseNoOfStudents;
+    DatabaseReference referenceStudent, referenceStudentNoOfCourses, referenceStudentCourses;
+    DatabaseReference referenceCourse, referenceCourseNoOfStudents, referenceCourseStudents;
     FirebaseUser user;
 
     EditText editTXTCoursePassCode;
@@ -46,33 +47,82 @@ public class CourseEnrollActivity extends AppCompatActivity {
         user = auth.getCurrentUser();
         rootNode = FirebaseDatabase.getInstance();
         referenceStudent = rootNode.getReference("students/"+ user.getUid());
-        referenceStudentNoOfCourses = rootNode.getReference("students/"+ user.getUid()+"/noOfCourses");
         referenceStudentCourses = rootNode.getReference("students/"+ user.getUid()+"/courses");
+        referenceStudentNoOfCourses = rootNode.getReference("students/"+user.getUid()+"/noOfCourses");
         referenceCourse = rootNode.getReference("courses");
-
         editTXTCoursePassCode = findViewById(R.id.edittxtcoursepasscode);
         enrollbtn = findViewById(R.id.enrollbtn);
         cancelbtn = findViewById(R.id.cancelbtn);
+
+        final Long[] noOfCourses = new Long[1];
+        referenceStudent.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                StudentHelperClass value = dataSnapshot.getValue(StudentHelperClass.class);
+                assert value != null;
+                noOfCourses[0] = (value.getNoOfCourses()) + 1L;
+                System.out.println("inside noc");
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Log.w("Error", "somethings wrong");
+            }
+        });
 
         cancelbtn.setOnClickListener(v -> {
             finish();
         });
         enrollbtn.setOnClickListener(v -> {
-            final boolean[] canEnroll = {false};
-            final Long[] noOfCourses = new Long[1];
             final Long[] noOfStudents = new Long[1];
             String coursePassCode = editTXTCoursePassCode.getText().toString().trim();
             if(ValidationChecker.isFieldEmpty(coursePassCode, editTXTCoursePassCode))return;
-            /*referenceCourse.child(coursePassCode).addListenerForSingleValueEvent(new ValueEventListener() {
+            referenceCourse.child(coursePassCode).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NotNull DataSnapshot snapshot) {
-                    if(!snapshot.exists()){
+                    if (!snapshot.exists()) {
                         editTXTCoursePassCode.setError("No course found");
                         editTXTCoursePassCode.requestFocus();
-                    }
-                    else {
-                        canEnroll[0] = true;
-                        System.out.println("sdfdsfdsf");
+                    } else {
+                        System.out.println("inside else");
+
+                        CourseHelperClass value = snapshot.getValue(CourseHelperClass.class);
+                        assert value != null;
+                        System.out.println(value.toString());
+                        System.out.println(value.getNoOfStudents());
+                        noOfStudents[0] = (value.getNoOfStudents()) + 1L;
+                        referenceCourseNoOfStudents = rootNode.getReference("courses/"+coursePassCode+"/noOfStudents");
+                        referenceCourseNoOfStudents.setValue((noOfStudents[0]), new DatabaseReference.CompletionListener() {
+
+                            @Override
+                            public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
+                                System.err.println("Value was set. Error = " + error);
+                            }
+                        });
+                        referenceStudentNoOfCourses.setValue((noOfCourses[0]), new DatabaseReference.CompletionListener() {
+
+                            @Override
+                            public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
+                                System.err.println("Value was set. Error = " + error);
+                            }
+                        });
+                        referenceStudentCourses.child(String.valueOf(noOfCourses[0])).setValue(coursePassCode, new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
+                                System.err.println("Value was set. Error = " + error);
+                            }
+                        });
+                        referenceCourseStudents = rootNode.getReference("courses/"+coursePassCode+"/students");
+                        referenceCourseStudents.child(String.valueOf(noOfStudents[0])).setValue(user.getUid(), new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
+                                System.err.println("Value was set. Error = " + error);
+                            }
+                        });
+                        Toast.makeText(getApplicationContext(), "Successfully Enrolled", Toast.LENGTH_SHORT).show();
+                        finish();
                     }
                 }
 
@@ -80,95 +130,11 @@ public class CourseEnrollActivity extends AppCompatActivity {
                 public void onCancelled(@NonNull @NotNull DatabaseError error) {
                     Log.w("Error","somethings wrong");
                 }
-            });*/
-
-            System.out.println("baire");
-            if(isCourseExists(coursePassCode)){
-                System.out.println("hidsfdsafdsfdsfdsfdsfdsfdssdfffffff");
-
-                referenceCourseNoOfStudents = rootNode.getReference("courses"+coursePassCode+"/noOfStudents");
-                referenceCourse.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
-                        CourseHelperClass value = dataSnapshot.getValue(CourseHelperClass.class);
-                        assert value != null;
-                        noOfStudents[0] = (value.getNoOfStudents()) + 1L;
-                    }
-
-                    @Override
-                    public void onCancelled(@NotNull DatabaseError error) {
-                        // Failed to read value
-                        Log.w("Error", "Failed to read value.", error.toException());
-                    }
-                });
-                referenceStudent.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
-                        StudentHelperClass value = dataSnapshot.getValue(StudentHelperClass.class);
-                        assert value != null;
-                        noOfCourses[0] = (value.getNoOfCourses()) + 1L;
-                    }
-
-                    @Override
-                    public void onCancelled(@NotNull DatabaseError error) {
-                        // Failed to read value
-                        Log.w("Error", "Failed to read value.", error.toException());
-                    }
-                });
-                referenceCourseNoOfStudents.setValue((noOfStudents[0]),new DatabaseReference.CompletionListener(){
-
-                    @Override
-                    public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
-                        System.err.println("Value was set. Error = "+error);
-                    }
-                });
-                referenceStudentNoOfCourses.setValue((noOfCourses[0]),new DatabaseReference.CompletionListener(){
-
-                    @Override
-                    public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
-                        System.err.println("Value was set. Error = "+error);
-                    }
-                });
-                String strNoOfCourses = String.valueOf(noOfCourses[0]);
-                referenceStudentCourses.child(strNoOfCourses).setValue(coursePassCode, new DatabaseReference.CompletionListener() {
-                    @Override
-                    public void onComplete(@Nullable @org.jetbrains.annotations.Nullable DatabaseError error, @NonNull @NotNull DatabaseReference ref) {
-                        System.err.println("Value was set. Error = "+error);
-                    }
-                });
-                Toast.makeText(getApplicationContext(), "Successfully Enrolled", Toast.LENGTH_SHORT).show();
-                finish();
-            }
+            });
+            //finish();
         });
     }
 
-    public boolean isCourseExists(String coursePassCode){
-        final boolean[] result = {false};
-        referenceCourse.child(coursePassCode).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NotNull DataSnapshot snapshot) {
-                if(!snapshot.exists()){
-                    editTXTCoursePassCode.setError("No course found");
-                    editTXTCoursePassCode.requestFocus();
-                    result[0] = false;
-                }
-                else {
-                    System.out.println("sdfdsfdsf");
-                    result[0] = true;
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull @NotNull DatabaseError error) {
-                Log.w("Error","somethings wrong");
-            }
-        });
-        return result[0];
-    }
     public boolean onCreateOptionsMenu(Menu menu){
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.profile_menu, menu);
