79630a2fc0da7c2b9501b9699af26babdeba85e7
tasnimcsedu25 <tasnimcsedu25@gmail.com>
2021-07-02 00:57:18 +0600

    profile picture upload done

--- app/src/main/java/com/example/zippy/ui/setting/ChangeProfilePictureActivity.java
+++ app/src/main/java/com/example/zippy/ui/setting/ChangeProfilePictureActivity.java
@@ -1,24 +1,152 @@
 package com.example.zippy.ui.setting;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.zippy.MainActivity;
 import com.example.zippy.R;
 import com.example.zippy.utility.NetworkChangeListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
 
 public class ChangeProfilePictureActivity extends AppCompatActivity {
 
     NetworkChangeListener networkChangeListener = new NetworkChangeListener();
+    private Button btnUpload, btnSubmit;
+    private ImageView imageView;
+    private DatabaseReference referenceProfilePicture;
+    private Uri filePath;
+    final String loggedStatus = "loggedProfile";
+    private String loggedProfile;
+
+    private final int PICK_IMAGE_REQUEST = 71;
+    //Firebase
+    FirebaseStorage storage;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_profile_picture);
+        SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        loggedProfile = mPrefs.getString(loggedStatus, "nouser");
+        btnUpload = (Button) findViewById(R.id.uploadbtn);
+        btnSubmit = (Button) findViewById(R.id.submitbtn );
+        imageView = (ImageView) findViewById(R.id.imgView);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        btnUpload.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                chooseImage();
+            }
+        });
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+        
     }
+    private void uploadImage() {
+
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+            FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+
+            if(loggedProfile.equals("student")) referenceProfilePicture = FirebaseDatabase.getInstance().getReference("students/"+user.getUid()+"/image");
+            else referenceProfilePicture = FirebaseDatabase.getInstance().getReference("instructors/"+user.getUid()+"/image");
+            StorageReference ref = storageReference.child("profilePictures/"+user.getUid()+"/profilePicture");
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+
+                            Toast.makeText(ChangeProfilePictureActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                            Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                            while (!uriTask.isSuccessful());
+                            String uploadedImageUrl = ""+uriTask.getResult();
+                            referenceProfilePicture.setValue(uploadedImageUrl);
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(ChangeProfilePictureActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
     public boolean onCreateOptionsMenu(Menu menu){
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.profile_menu, menu);
